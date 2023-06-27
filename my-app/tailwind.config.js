2cfb3e86409f3a8be1626b81861bff274046ac76
Kankina Tetiana <65289767+Klem26@users.noreply.github.com>
2023-06-27 22:57:52 +0300

    Initial commit

---
+++ my-app/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        "primary-blue": "#0891b2",
+      },
+    },
+  },
+  plugins: [],
+};
